9c9d34576b9c2edd11024e94c32ba852702a798b
francing-p <ngfranciscus@gmail.com>
2024-09-18 16:01:41 -0400

    Commit

--- src/maintenance.java
+++ src/maintenance.java
@@ -1,5 +1,14 @@
 public class maintenance {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double winterCost = 678;
+        double springCost = 331;
+        double summerCost = 450.5;
+        double fallCost = 230;
+        double totalCost = winterCost + springCost + summerCost + fallCost;
+        System.out.println("The cost of maintenance in winter was: $" + winterCost);
+        System.out.println("The cost of maintenance in spring was: $" + springCost);
+        System.out.println("The cost of maintenance in summer was: $" + summerCost);
+        System.out.println("The cost of maintenance in fall was: $" + fallCost);
+        System.out.println("The total cost of maintenance was: $" + totalCost);
     }
 }
